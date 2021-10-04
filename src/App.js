2a63a31821dd4417f450362e1b4ef0af2584386a
jaehokim <s2heart1@naver.com>
2021-10-04 19:56:14 +0900

    router 경로적용

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 import MainPage from './components/MainPage';
+import {Switch, Route} from 'react-router-dom';
+import UploadPage from './components/Upload';
+import ProducPage from './components/Product';
+
+
 function App() {
   return (
     <div className="App">
-         <MainPage></MainPage>
+        <Switch>
+          <Route exact={true} path ={"/"}>
+            <MainPage/>
+          </Route>
+          <Route exact={true} path ={"/product"}>
+            <ProducPage/>
+          </Route>
+          <Route exact={true} path ={"/upload"}>
+            <UploadPage/>
+          </Route>
+         </Switch>
     </div>
   );
 }
---
+++ src/components/Product.jsx
@@ -0,0 +1,4 @@
+function ProducPage() {
+    return <h1> 상품상세 페이지</h1>
+}
+export default ProducPage;
